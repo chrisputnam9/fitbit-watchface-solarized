1f0e9b571aab1da115666522d2282260fa32e601
Chris Putnam <chris@webfx.com>
2023-04-13 08:38:13 -0400

    Dynamic location for weather functional

--- companion/index.js
+++ companion/index.js
@@ -23,7 +23,7 @@ geolocation.watchPosition(
 
 // Listen for the onopen event
 messaging.peerSocket.onopen = function () {
-	console.log( 'peerSocket onopen' );
+	console.log( 'peerSocket.onopen - will send hosted data to watch' );
 	// Fetch and send hosted data response
 	sendHostedData();
 };
@@ -31,26 +31,27 @@ messaging.peerSocket.onopen = function () {
 // Listen for the onmessage event
 messaging.peerSocket.onmessage = function ( evt ) {
 	// Output the message to the console
-	console.log( 'peerSocket onmessage' );
-	console.log( JSON.stringify( evt.data ) );
+	console.log( 'peerSocket.onmessage: ', JSON.stringify( evt.data ) );
 	if ( 'request' in evt.data ) {
 		if ( evt.data.request === 'hosted_data' ) {
+			console.log(
+				' - hosted_data requested - will send hosted data to watch'
+			);
 			sendHostedData();
 		}
 	}
 };
 
 function sendHostedData() {
+	console.log( 'sendHostedData:' );
 	const server_url_json = settingsStorage.getItem( 'server_url' );
 	const server_url_data = JSON.parse( server_url_json );
 	let server_url =
 		server_url_data && 'name' in server_url_data
 			? server_url_data.name
 			: false;
-	if ( server_url ) {
-		console.log( 'fetching from server_url: ' + server_url );
-	} else {
-		console.log( 'ERROR: Check settings - server url is empty' );
+	if ( ! server_url ) {
+		console.log( ' - ERROR: Check settings - server url is empty' );
 		return;
 	}
 
@@ -61,23 +62,25 @@ function sendHostedData() {
 			server_url += '&lng=' + geo_position.coords.longitude;
 		}
 
+		console.log( ' - Fetching from server_url: ' + server_url );
 		fetch( server_url )
 			.then( ( response ) => response.json() )
 			.then( ( data ) => {
-				console.log( 'response from server: ' );
-				console.log( data );
+				console.log(
+					' - Response from server: ',
+					JSON.stringify( data )
+				);
 				if (
 					messaging.peerSocket.readyState ===
 					messaging.peerSocket.OPEN
 				) {
 					messaging.peerSocket.send( data );
 				} else {
-					console.log( 'ERROR: Messaging connection is not open' );
+					console.log( ' - ERROR: Messaging connection is not open' );
 				}
 			} )
 			.catch( function ( e ) {
-				console.log( 'ERROR:' );
-				console.log( e );
+				console.log( ' - ERROR:', JSON.stringify( e ) );
 			} );
 	}
 }
