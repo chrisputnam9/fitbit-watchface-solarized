3acf308d46663c47620d1497a71cee28b81e32d8
Chris Putnam <chris@webfx.com>
2023-04-15 11:05:52 -0400

    Fix for getting geolocation data

--- companion/index.js
+++ companion/index.js
@@ -3,29 +3,12 @@ import * as messaging from 'messaging';
 import { geolocation } from 'geolocation';
 
 console.log( 'Companion JS running' );
-let geo_position;
-
-geolocation.watchPosition(
-	( fresh_geo_position ) => {
-		geo_position = fresh_geo_position;
-	},
-	( error ) => {
-		console.log(
-			'Error during watchPosition: ' + error.code + ': ' + error.message
-		);
-	},
-	{
-		enableHighAccuracy: true,
-		maximumAge: 60000, // 1 hr cache
-		timeout: 60000, // 1 hr timeout
-	}
-);
 
 // Listen for the onopen event
 messaging.peerSocket.onopen = function () {
 	console.log( 'peerSocket.onopen - will send hosted data to watch' );
 	// Fetch and send hosted data response
-	sendHostedData();
+	maybeSendHostedData();
 };
 
 // Listen for the onmessage event
@@ -37,16 +20,16 @@ messaging.peerSocket.onmessage = function ( evt ) {
 			console.log(
 				' - hosted_data requested - will send hosted data to watch'
 			);
-			sendHostedData();
+			maybeSendHostedData();
 		}
 	}
 };
 
-function sendHostedData() {
-	console.log( 'sendHostedData:' );
+function maybeSendHostedData() {
+	console.log( 'maybeSendHostedData:' );
 	const server_url_json = settingsStorage.getItem( 'server_url' );
 	const server_url_data = JSON.parse( server_url_json );
-	let server_url =
+	const server_url =
 		server_url_data && 'name' in server_url_data
 			? server_url_data.name
 			: false;
@@ -55,32 +38,66 @@ function sendHostedData() {
 		return;
 	}
 
-	if ( server_url ) {
-		// Add geo location to request data -  if we have it
-		if ( 'coords' in geo_position ) {
-			server_url += '&lat=' + geo_position.coords.latitude;
-			server_url += '&lng=' + geo_position.coords.longitude;
-		}
+	addGeoPosition( server_url );
+}
 
-		console.log( ' - Fetching from server_url: ' + server_url );
-		fetch( server_url )
-			.then( ( response ) => response.json() )
-			.then( ( data ) => {
+/**
+ * Try and add geolocated position to server URL
+ * - Either way, continue on to send hosted data
+ */
+function addGeoPosition( server_url ) {
+	console.log( 'addGeoPosition(' + server_url + ')' );
+	geolocation.getCurrentPosition(
+		( geo_position ) => {
+			if (
+				geo_position &&
+				typeof geo_position === 'object' &&
+				'coords' in geo_position &&
+				'latitude' in geo_position.coords
+			) {
+				server_url += '&lat=' + geo_position.coords.latitude;
+				server_url += '&lng=' + geo_position.coords.longitude;
+			}
+			sendHostedData( server_url );
+		},
+		( error ) => {
+			console.log(
+				' - ERROR during geolocation.getCurrentPosition: ' +
+					error.code +
+					': ' +
+					error.message,
+				'Continuing without geolocation data'
+			);
+			sendHostedData( server_url );
+		},
+		{
+			enableHighAccuracy: false,
+			maximumAge: 3600000, // 1 hr cache = 60 * 60 * 1000 ms
+			timeout: 5000, // 5 second timeout = 5 * 1000 ms
+		}
+	);
+}
+
+function sendHostedData( server_url ) {
+	console.log( 'sendHostedData(' + server_url + ')' );
+	fetch( server_url )
+		.then( ( response ) => response.json() )
+		.then( ( data ) => {
+			console.log( ' - Response from server: ', JSON.stringify( data ) );
+			if (
+				messaging.peerSocket.readyState === messaging.peerSocket.OPEN
+			) {
+				messaging.peerSocket.send( data );
+			} else {
 				console.log(
-					' - Response from server: ',
-					JSON.stringify( data )
+					' - ERROR sending to device: Messaging connection is not open'
 				);
-				if (
-					messaging.peerSocket.readyState ===
-					messaging.peerSocket.OPEN
-				) {
-					messaging.peerSocket.send( data );
-				} else {
-					console.log( ' - ERROR: Messaging connection is not open' );
-				}
-			} )
-			.catch( function ( e ) {
-				console.log( ' - ERROR:', JSON.stringify( e ) );
-			} );
-	}
+			}
+		} )
+		.catch( function ( e ) {
+			console.log(
+				' - ERROR fetching from server:',
+				JSON.stringify( e )
+			);
+		} );
 }
